10a558f09404f58cb4ece7dca64d8e0313f7aa1f
Medan <medan.dev@ya.ru>
2022-11-16 20:43:59 +0400

    Readers to static

--- src/panels/MainPanel/CubeMxAdapterPanel.ts
+++ src/panels/MainPanel/CubeMxAdapterPanel.ts
@@ -11,12 +11,13 @@ export class CubeMxAdapterPanel {
   public static currentPanel: CubeMxAdapterPanel | undefined;
   private readonly _panel: vscode.WebviewPanel;
   private _disposables: vscode.Disposable[] = [];
-  private makefileReader : MakefileReader;
-  private cCppPropReader : cCppPropertiesReader;
-  private debugLaunchReader : DebugLaunchReader;
-  private mainConfigJson : MainConfigJson;
   private workspacePath : string = '';
   private svdFilesList : string[] = [];
+
+  public static makefileReader : MakefileReader;
+  public static cCppPropReader : cCppPropertiesReader;
+  public static debugLaunchReader : DebugLaunchReader;
+  public static mainConfigJson : MainConfigJson;
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
   private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
@@ -29,10 +30,10 @@ export class CubeMxAdapterPanel {
       this.workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
     }
 
-    this.makefileReader = new MakefileReader(this.workspacePath + "/Makefile");
-    this.cCppPropReader = new cCppPropertiesReader(this.workspacePath + "/.vscode/c_cpp_properties.json", this.makefileReader);
-    this.debugLaunchReader = new DebugLaunchReader(this.workspacePath + "/.vscode/launch.json", this.makefileReader)
-    this.mainConfigJson = new MainConfigJson();
+    CubeMxAdapterPanel.makefileReader = new MakefileReader(this.workspacePath + "/Makefile");
+    CubeMxAdapterPanel.cCppPropReader = new cCppPropertiesReader(this.workspacePath + "/.vscode/c_cpp_properties.json", CubeMxAdapterPanel.makefileReader);
+    CubeMxAdapterPanel.debugLaunchReader = new DebugLaunchReader(this.workspacePath + "/.vscode/launch.json", CubeMxAdapterPanel.makefileReader)
+    CubeMxAdapterPanel.mainConfigJson = new MainConfigJson();
 
     svdDownloader.getListOfSvdFiles().then(svdList => {
       this.svdFilesList = svdList;
@@ -133,7 +134,7 @@ export class CubeMxAdapterPanel {
             return;
 
           case "defines_clickAddButton":
-            // this.openHeaderDialog();
+            // TODO David will implement
             return;
 
           case "getAllMakefileInformation":
@@ -141,32 +142,32 @@ export class CubeMxAdapterPanel {
             return;
 
           case "cSrcFiles_clickDeleteButton":
-            this.makefileReader.deleteValuesInVariable(
-              this.makefileReader.cSourceMakeVar,
+            CubeMxAdapterPanel.makefileReader.deleteValuesInVariable(
+              CubeMxAdapterPanel.makefileReader.cSourceMakeVar,
               text.split(",")
             );
             this.sendAllVariablesToUi();
             return;
 
           case "cppSrcFiles_clickDeleteButton":
-            this.makefileReader.deleteValuesInVariable(
-              this.makefileReader.cppSourceMakeVar,
+            CubeMxAdapterPanel.makefileReader.deleteValuesInVariable(
+              CubeMxAdapterPanel.makefileReader.cppSourceMakeVar,
               text.split(",")
             );
             this.sendAllVariablesToUi();
             return;
 
           case "incFolders_clickDeleteButton":
-            this.makefileReader.deleteValuesInVariable(
-              this.makefileReader.cIncludeMakeVar,
+            CubeMxAdapterPanel.makefileReader.deleteValuesInVariable(
+              CubeMxAdapterPanel.makefileReader.cIncludeMakeVar,
               text.split(",")
             );
             this.sendAllVariablesToUi();
             return;
 
           case "defines_clickDeleteButton":
-            this.makefileReader.deleteValuesInVariable(
-              this.makefileReader.cDefineMakeVar,
+            CubeMxAdapterPanel.makefileReader.deleteValuesInVariable(
+              CubeMxAdapterPanel.makefileReader.cDefineMakeVar,
               text.split(",")
             );
             this.sendAllVariablesToUi();
@@ -200,10 +201,10 @@ export class CubeMxAdapterPanel {
         this.sendMsg(cmd, newPath);
 
         if(title === "Compiler path") {
-          this.mainConfigJson.setCompilerPath(newPath);
+          CubeMxAdapterPanel.mainConfigJson.setCompilerPath(newPath);
         }
         else if (title === "Openocd path") {
-          this.mainConfigJson.setOpenocdPath(newPath);
+          CubeMxAdapterPanel.mainConfigJson.setOpenocdPath(newPath);
         }
       }
     });
@@ -211,6 +212,7 @@ export class CubeMxAdapterPanel {
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
   private openCSourceDialog() {
+    let _makefileReader = CubeMxAdapterPanel.makefileReader;
     const opt: vscode.OpenDialogOptions = {
       filters: { "C Source": ["c"] },
       canSelectMany: true,
@@ -227,11 +229,11 @@ export class CubeMxAdapterPanel {
           list.push(newRelativePath);
         }
 
-        let existList = this.makefileReader.getVariableList(this.makefileReader.cSourceMakeVar);
+        let existList = _makefileReader.getVariableList(_makefileReader.cSourceMakeVar);
         list = this.exeptCompareItems(existList, list);
         if(list.length !== 0) {
           this.sendMsgAddPaths("cSrcFiles_addNewLines", list);
-          this.makefileReader.addValuesInVariable(this.makefileReader.cSourceMakeVar, list);
+          _makefileReader.addValuesInVariable(_makefileReader.cSourceMakeVar, list);
         }
       }
     });
@@ -239,6 +241,7 @@ export class CubeMxAdapterPanel {
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
   private openCppSourceDialog() {
+    let _makefileReader = CubeMxAdapterPanel.makefileReader;
     const opt: vscode.OpenDialogOptions = {
       filters: { "C++ Source": ["cpp"] },
       canSelectMany: true,
@@ -253,12 +256,12 @@ export class CubeMxAdapterPanel {
           list.push(newRelativePath);
         }
 
-        if(this.makefileReader.checkExistVariable(this.makefileReader.cppSourceMakeVar)) {
-          let existList = this.makefileReader.getVariableList(this.makefileReader.cppSourceMakeVar);
+        if(_makefileReader.checkExistVariable(_makefileReader.cppSourceMakeVar)) {
+          let existList = _makefileReader.getVariableList(_makefileReader.cppSourceMakeVar);
           list = this.exeptCompareItems(existList, list);
           if(list.length !== 0) {
             this.sendMsgAddPaths("cppSrcFiles_addNewLines", list);
-            this.makefileReader.addValuesInVariable(this.makefileReader.cppSourceMakeVar, list);
+            _makefileReader.addValuesInVariable(_makefileReader.cppSourceMakeVar, list);
           }
         }        
       }
@@ -267,6 +270,7 @@ export class CubeMxAdapterPanel {
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
   private openHeaderDialog() {
+    let _makefileReader = CubeMxAdapterPanel.makefileReader;
     const opt: vscode.OpenDialogOptions = {
       filters: {},
       canSelectMany: true,
@@ -284,12 +288,12 @@ export class CubeMxAdapterPanel {
           list.push(newRelativePath);
         }
 
-        let existList = this.makefileReader.getVariableList(this.makefileReader.cIncludeMakeVar);
+        let existList = _makefileReader.getVariableList(_makefileReader.cIncludeMakeVar);
         existList = existList.map((value) => value.replace(/-I/g, ''));
         list = this.exeptCompareItems(existList, list);
         if(list.length !== 0) {
           this.sendMsgAddPaths("incFolders_addNewLines", list);
-          this.makefileReader.addValuesInVariableWithPrefix(this.makefileReader.cIncludeMakeVar, "-I", list);
+          _makefileReader.addValuesInVariableWithPrefix(_makefileReader.cIncludeMakeVar, "-I", list);
           // TODO Add into c/cpp prop
         }
       }
@@ -298,20 +302,21 @@ export class CubeMxAdapterPanel {
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
   private adaptForVSC() {
-    this.makefileReader.activateSilentMode();
-    this.makefileReader.activateEchoForCompilation();
-    this.cCppPropReader.InitNewConfiguration();
-    this.debugLaunchReader.InitNewConfiguration();
+    CubeMxAdapterPanel.makefileReader.activateSilentMode();
+    CubeMxAdapterPanel.makefileReader.activateEchoForCompilation();
+    CubeMxAdapterPanel.cCppPropReader.InitNewConfiguration();
+    CubeMxAdapterPanel.debugLaunchReader.InitNewConfiguration();
   }
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
   private adaptForCpp() {
-    this.makefileReader.addCppCompilerVar();
-    this.makefileReader.addNewVariableAfter(this.makefileReader.cSourceMakeVar, this.makefileReader.cppSourceMakeVar);
-    this.makefileReader.addCppCompilerFlags();
-    this.makefileReader.addCppObjectsVar();
-    this.makefileReader.addCppCompileTask();
-    this.makefileReader.changeLinker();
+    let _makefileReader = CubeMxAdapterPanel.makefileReader;
+    _makefileReader.addCppCompilerVar();
+    _makefileReader.addNewVariableAfter(_makefileReader.cSourceMakeVar, _makefileReader.cppSourceMakeVar);
+    _makefileReader.addCppCompilerFlags();
+    _makefileReader.addCppObjectsVar();
+    _makefileReader.addCppCompileTask();
+    _makefileReader.changeLinker();
   }
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
@@ -333,17 +338,19 @@ export class CubeMxAdapterPanel {
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
   private sendAllVariablesToUi() {
+    let _makefileReader = CubeMxAdapterPanel.makefileReader;
     this.sendCmd("cSrcFiles_allClear");
     this.sendCmd("cppSrcFiles_allClear");
     this.sendCmd("incFolders_allClear");
     this.sendCmd("defines_allClear");
-    this.sendMsgAddPaths("cSrcFiles_addNewLines", this.makefileReader.getCSourcePaths());
-    this.sendMsgAddPaths("cppSrcFiles_addNewLines", this.makefileReader.getCppSourcePaths());
-    this.sendMsgAddPaths("incFolders_addNewLines", this.makefileReader.getIncludePaths());
-    this.sendMsgAddPaths("defines_addNewLines", this.makefileReader.getDefines());
+    this.sendMsgAddPaths("cSrcFiles_addNewLines", _makefileReader.getCSourcePaths());
+    this.sendMsgAddPaths("cppSrcFiles_addNewLines", _makefileReader.getCppSourcePaths());
+    this.sendMsgAddPaths("incFolders_addNewLines", _makefileReader.getIncludePaths());
+    this.sendMsgAddPaths("defines_addNewLines", _makefileReader.getDefines());
 
     this.sendMsg("toolChain_UpdatePaths", 
-      JSON.stringify({ compilerPath: this.mainConfigJson.getCompilerPath(), openocdPath: this.mainConfigJson.getOpenocdPath()}));
+                JSON.stringify({ compilerPath: CubeMxAdapterPanel.mainConfigJson.getCompilerPath(), 
+                                openocdPath: CubeMxAdapterPanel.mainConfigJson.getOpenocdPath()}));
   }
 
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
